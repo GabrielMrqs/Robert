d465bda3fc7dc302d4293a1bfb347273eaecb012
Gabriel Marques <gabrielmrqs.contato@gmail.com>
2023-01-02 01:42:09 -0300

    limpezinha nem gosto di certo

--- RobertMQ.Application/SuperMessage.cs
+++ RobertMQ.Application/SuperMessage.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RobertMQ.Application
+﻿namespace RobertMQ.Application
 {
     public class SuperMessage : ICommand
     {
--- RobertMQ.ReceiverWorker/SuperMessageHandler.cs
+++ RobertMQ.ReceiverWorker/SuperMessageHandler.cs
@@ -1,10 +1,5 @@
 ﻿using NServiceBus;
 using RobertMQ.Application;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RobertMQ.ReceiverWorker
 {
